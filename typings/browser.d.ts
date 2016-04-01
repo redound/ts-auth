9fca00683b6da8ba979a6fdacfcb86e2f3f58fa1
Olivier Andriessen <olivierandriessen@gmail.com>
2016-04-01 14:43:29 +0200

    Recompile

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,4 +1,3 @@
 /// <reference path="browser/ambient/angular/index.d.ts" />
 /// <reference path="browser/ambient/jquery/index.d.ts" />
 /// <reference path="browser/ambient/underscore/index.d.ts" />
-/// <reference path="browser/definitions/ts-core/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,4 +1,3 @@
 /// <reference path="main/ambient/angular/index.d.ts" />
 /// <reference path="main/ambient/jquery/index.d.ts" />
 /// <reference path="main/ambient/underscore/index.d.ts" />
-/// <reference path="main/definitions/ts-core/index.d.ts" />
